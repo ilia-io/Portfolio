1e2c24461031d1145668a12e382b87c7ee1dd27c
ilia-io <likeapu@gmail.com>
2022-07-21 20:08:56 +0300

    add navigation bar and icons

---
+++ src/components/About.jsx
@@ -0,0 +1,9 @@
+function About() {
+  return (
+    <div name="about" className="w-full h-screen bg-[#0a192f] text-gray-300">
+      <div className="flex flex-col justify-center items-center w-full h-full"></div>
+    </div>
+  );
+}
+
+export default About;
