977900dc8aad52b9c68f963270186c8c0b4c1359
ilia-io <likeapu@gmail.com>
2022-09-24 12:22:06 +0300

    fix: media

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,19 +12,12 @@ function Footer() {
           className: 'cursor-pointer',
         }}
       >
-        <div className="flex justify-between items-center p-4 min-w-[220px] ">
+        <div className="flex justify-around items-center p-4 min-w-[220px] ">
           <a href="https://t.me/ilia_i0" target="_blank" rel="noreferrer">
             <FaTelegram />
           </a>
           <a href="https://github.com/ilia-io" target="_blank" rel="noreferrer">
             <FaGithub />
-          </a>
-          <a
-            href="https://career.habr.com/ilia-io"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <SiHabr />
           </a>
         </div>
       </IconContext.Provider>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -115,16 +115,6 @@ function Navbar() {
       {/* social icons */}
       <div className="hidden lg:flex fixed flex-col top-[35%] left-0 ">
         <ul>
-          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6274BC]">
-            <a
-              className="flex justify-between items-center w-full text-gray-100 text-sm"
-              href="https://career.habr.com/ilia-io"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Habr career <SiHabr size={33} />
-            </a>
-          </li>
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#222]">
             <a
               className="flex justify-between items-center w-full text-gray-200 text-sm"
